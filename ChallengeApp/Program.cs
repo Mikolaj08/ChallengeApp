8979b43be880e78866a39899c82ca93f9f5155c9
mikol <mikol@DESKTOP-4KFD46N>
2023-03-16 14:01:38 +0100

    zadanie nr 4

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.VisualBasic.FileIO;
+using System.ComponentModel.Design;
+
+string name = "Ewa";
+string sex = "woman";
+var age = 33;
+
+//1. Kobieta poniżej 30 lat
+if (sex == "woman" && age < 30);
+{
+    Console.WriteLine("kobieta poniżej 30lat");
+}
+
+//2. Ewa lat 33
+if (name == "Ewa" && age == 33);
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+
+//3. Niepełnoletni mężczyzna
+if (!(sex == "woman" && age > 18));
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
